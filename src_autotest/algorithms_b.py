0e0299276f6cdaa65fccbd46dbc5d35e5b32ac2c
Sheng <ws83@students.uni-freiburg.de>
2026-03-10 04:40:49 -0700

    add ablation autotest

---
+++ src_autotest/algorithms_b.py
@@ -0,0 +1,384 @@
+import numpy as np
+import time
+import torch
+import torch.nn as nn
+
+from scipy.special import logsumexp
+from model.intention_b import IntentionRNN, IntentionLSTM, IntentionTransformer
+from torch.utils.data import DataLoader, TensorDataset
+
+
+class IAVI_B:
+    def __init__(self, num_states, num_actions, P, expert_policy, discount, threshold=1e-3, alpha=0.1, device='cuda'):
+        self.num_states = num_states
+        self.num_actions = num_actions
+        self.device = torch.device(device if torch.cuda.is_available() else 'cpu')
+        self.P = P
+        self.expert_policy = torch.as_tensor(expert_policy, dtype=torch.float64, device=self.device)
+        self.discount = discount
+        self.threshold = threshold
+        self.epsilon = 1e-6
+        self.alpha = alpha
+        self.batch_size = num_states
+
+        self.r = torch.randn(self.num_states, self.num_actions, dtype=torch.float64, device=self.device)
+        self.q = torch.randn(self.num_states, self.num_actions, dtype=torch.float64, device=self.device)
+
+        X = torch.full((self.num_actions, self.num_actions), -1 / (self.num_actions - 1), dtype=torch.float64, device=self.device)
+        X.fill_diagonal_(1.0)
+        self.X = X.cpu().numpy()
+
+    def train(self):
+        e = 0
+        while e < 1e5:
+            e += 1
+            delta = 0
+            # sampled_indices = torch.randperm(self.num_states, device=self.device)[:self.batch_size]
+            for start_idx in range(0, self.num_states, self.batch_size):
+                end_idx = min(start_idx + self.batch_size, self.num_states)
+                sampled_indices = torch.arange(start_idx, end_idx, device=self.device)
+
+                tp_batch = self.P[sampled_indices]  # (batch_size, num_actions, num_states)
+                expert_policy_batch = self.expert_policy[sampled_indices]  # (batch_size, num_actions)
+                max_q = torch.max(self.q, dim=1).values
+                opt_nextv_batch = self.discount * torch.matmul(tp_batch, max_q.unsqueeze(-1)).squeeze(-1)
+                eta_batch = torch.log(expert_policy_batch + self.epsilon) - opt_nextv_batch  # (batch_size, num_actions)
+                if not torch.all(torch.isfinite(eta_batch)):
+                    print("Non-finite eta detected!")
+
+                eta_sum = eta_batch.sum(dim=1, keepdim=True)
+                Y_batch = eta_batch - (eta_sum - eta_batch) / (self.num_actions - 1)  # (batch_size, num_actions)
+
+                # must convert to numpy for lstsq
+                tX = self.X
+                tY = Y_batch.cpu().numpy()
+                tr = np.linalg.lstsq(tX, tY.T, rcond=None)[0] # perform worse when using torch.linalg.lstsq. reason unclear
+
+                r_new_batch = torch.as_tensor(tr, dtype=torch.float64, device=self.device).T 
+                delta_batch = torch.max(torch.abs(self.r[sampled_indices] - r_new_batch)).item()
+                delta = max(delta, delta_batch)
+
+                self.r[sampled_indices] = self.alpha * self.r[sampled_indices] + (1 - self.alpha) * r_new_batch
+                self.q[sampled_indices] = self.alpha * self.q[sampled_indices] + (1 - self.alpha) * (self.r[sampled_indices] + opt_nextv_batch)
+
+            if delta < self.threshold:
+                break
+
+        if e >= 1e5:
+            raise RuntimeError("IAVI did not converge within the maximum number of iterations.")
+        
+        return delta
+    
+    def get_policy(self):
+        return torch.softmax(self.q, dim=-1).cpu().numpy()
+    
+    def get_q_values(self):
+        return self.q.cpu().numpy()
+    
+    def get_rewards(self):
+        return self.r.cpu().numpy()
+
+
+class PGIAVI_B:
+    def __init__(self, num_latents, num_states, num_actions, P, train_trajs, test_trajs, discount,
+                 model_type='IntentionRNN', hidden_dim=128, rnn_hidden_dim=128,
+                 num_layers=1, dropout=0.3, nhead=4, lr=1e-3,
+                 reg_type='l1', reg_weight=0.35, num_epochs=3,
+                 loss_threshold=5e-2, max_iterations=120):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.num_latents = num_latents
+        self.num_states = num_states
+        self.num_actions = num_actions
+        self.P = torch.as_tensor(P, dtype=torch.float64, device=self.device)
+        self.discount = discount
+        self.train_trajs = train_trajs
+        self.test_trajs = test_trajs
+
+        self.reg_type = reg_type
+        self.reg_weight = reg_weight
+        self.num_epochs = num_epochs
+        self.loss_threshold = loss_threshold
+        self.max_iterations = max_iterations
+
+        model_kwargs = dict(num_states=num_states, num_actions=num_actions,
+                            num_latents=num_latents, num_layers=num_layers, dropout=dropout)
+        if model_type == 'IntentionTransformer':
+            model_kwargs.update(d_model=hidden_dim, nhead=nhead)
+            model_cls = IntentionTransformer
+        elif model_type == 'IntentionLSTM':
+            model_kwargs.update(hidden_dim=hidden_dim, rnn_hidden_dim=rnn_hidden_dim)
+            model_cls = IntentionLSTM
+        else:
+            model_kwargs.update(hidden_dim=hidden_dim, rnn_hidden_dim=rnn_hidden_dim)
+            model_cls = IntentionRNN
+
+        self.intention_net = model_cls(**model_kwargs).to(self.device)
+        self.target_intention_net = model_cls(**model_kwargs).to(self.device)
+        self.target_intention_net.load_state_dict(self.intention_net.state_dict())
+        self.target_intention_net.eval()
+        self.optimizer = torch.optim.Adam(self.intention_net.parameters(), lr=lr)
+        
+
+    def intention_batch_mapping(self, e_loader, total_length):
+        log_p_gammas = []
+        log_fs = []
+        log_joints = []
+        with torch.no_grad():
+            for batch_states, batch_actions, batch_log_pi, batch_mask in e_loader:
+                f_logits = self.target_intention_net(
+                    batch_states.to(self.device),
+                    batch_actions.to(self.device),
+                    mask=batch_mask.to(self.device),
+                    total_length=total_length).to('cpu')       # (B, T_max, K)
+                log_f = torch.log_softmax(f_logits, dim=-1)
+                log_joint = log_f + batch_log_pi
+                log_p_gamma = log_joint - torch.logsumexp(log_joint, dim=-1, keepdim=True) # (B, T_max, K)
+
+                if batch_mask is not None:
+                    log_p_gamma = log_p_gamma * batch_mask.unsqueeze(-1)
+                    log_f = log_f * batch_mask.unsqueeze(-1)
+                    log_joint = log_joint * batch_mask.unsqueeze(-1)
+
+                log_p_gammas.append(log_p_gamma)
+                log_fs.append(log_f)
+                log_joints.append(log_joint)
+
+        return torch.concatenate(log_p_gammas, axis=0).to('cpu'), \
+               torch.concatenate(log_fs, axis=0).to('cpu'), \
+               torch.concatenate(log_joints, axis=0).to('cpu')
+
+    def get_batch_log_pi(self, trajs, agents):
+        agent_policies = []
+        for agent in agents:
+            pi = torch.softmax(agent.q.to('cpu'), dim=-1)  # Boltzmann policy
+            agent_policies.append(pi)
+        agent_policies = torch.stack(agent_policies, dim=0)  # (K, num_states, num_actions)
+
+        log_pi_list = []
+        seq_lens = []
+        for traj in trajs:
+            T = len(traj)
+            seq_lens.append(T)
+
+            states = torch.tensor([s for s, a, ns in traj], dtype=torch.long, device='cpu')
+            actions = torch.tensor([a for s, a, ns in traj], dtype=torch.long, device='cpu')
+            log_pi = torch.log(agent_policies[:, states, actions] + 1e-8)  # (K, T)
+            log_pi_list.append(log_pi.T)  # (T, K)
+
+        # Padding
+        max_len = max(seq_lens)
+        batch_log_pi = torch.zeros((len(trajs), max_len, self.num_latents), dtype=torch.float32, device='cpu')
+        for i, log_pi in enumerate(log_pi_list):
+            batch_log_pi[i, :seq_lens[i], :] = log_pi
+        
+        return batch_log_pi
+
+    def encode_session_traj(self, traj):
+        states = torch.tensor([s for s, a, ns in traj], dtype=torch.long)
+        actions = torch.tensor([a for s, a, ns in traj], dtype=torch.long)
+        return states, actions
+
+    def encode_batch_trajs(self, trajs):
+        batch_states_list = []
+        batch_actions_list = []
+        seq_lens = []
+        for traj in trajs:
+            states, actions = self.encode_session_traj(traj)
+            batch_states_list.append(states)
+            batch_actions_list.append(actions)
+            seq_lens.append(states.shape[0])
+
+        max_len = max(seq_lens)
+        batch_states = torch.zeros((len(trajs), max_len), dtype=torch.long, device='cpu')
+        batch_actions = torch.zeros((len(trajs), max_len), dtype=torch.long, device='cpu')
+        mask = torch.zeros((len(trajs), max_len), dtype=torch.bool, device='cpu')
+        for i, (states, actions) in enumerate(zip(batch_states_list, batch_actions_list)):
+            seq_len = seq_lens[i]
+            batch_states[i, :seq_len] = states
+            batch_actions[i, :seq_len] = actions
+            mask[i, :seq_len] = 1
+
+        return batch_states, batch_actions, mask
+    
+    def train_minibatch(self, m_loader, total_length, num_epochs=1, reg_weight=0., reg_type='l1'):
+        loss_list = []
+        for epoch in range(num_epochs):
+            total_loss = 0
+            for batch_states, batch_actions, batch_target_gamma, batch_mask in m_loader:
+                batch_states = batch_states.to(self.device)
+                batch_actions = batch_actions.to(self.device)
+                batch_target_gamma = batch_target_gamma.to(self.device)
+                batch_mask = batch_mask.to(self.device)
+
+                self.optimizer.zero_grad()
+
+                pred_logits = self.intention_net(batch_states, batch_actions, mask=batch_mask, total_length=total_length)
+                pred_logf = torch.log_softmax(pred_logits, dim=-1)
+
+                nll_loss = -(batch_target_gamma * pred_logf * batch_mask.unsqueeze(-1)).sum(dim=-1).mean()
+
+                if reg_weight > 0.:
+                    mask_curr = batch_mask[:, 1:]
+                    gamma_curr = batch_target_gamma[:, 1:, :]
+
+                    f_curr = pred_logits[:, 1:, :].softmax(dim=-1)
+                    f_prev = pred_logits[:, :-1, :].softmax(dim=-1)
+
+                    l1 = gamma_curr * torch.abs(f_curr - f_prev)
+                    l1 = (l1.sum(dim=-1) * mask_curr).mean()
+                    kl = f_prev * (torch.log(f_prev + 1e-8) - torch.log(f_curr + 1e-8))
+                    kl = (kl.sum(dim=-1) * mask_curr).mean()
+
+                    reg = l1 if reg_type == 'l1' else kl
+                    kl_reg = reg_weight * reg
+                else:
+                    kl_reg = 0.
+
+                loss = nll_loss + kl_reg
+                loss.backward()
+                self.optimizer.step()
+                total_loss += loss.item()
+            loss_list.append(total_loss)
+        
+        return np.mean(loss_list)
+    
+    def fit(self):
+        # * * * Initialize agents with uniform policy * * *
+        uniform_policy = np.full((self.num_states, self.num_actions), 1.0 / self.num_actions)
+        agents = []
+        for _ in range(self.num_latents):
+            agent = IAVI_B(
+                num_states=self.num_states,
+                num_actions=self.num_actions,
+                P=self.P,
+                expert_policy=uniform_policy,
+                discount=self.discount,
+                device=self.device
+            )
+            agent.train()
+            agents.append(agent)
+
+        # * * * Encode train trajs * * *
+        batch_states, batch_actions, batch_mask = self.encode_batch_trajs(self.train_trajs)
+        max_len = batch_states.shape[1]
+
+        logger_cnt = 0
+        logstep_exp_time = 0
+        logstep_q_time = 0
+        logstep_intention_time = 0
+        iteration_start_time = time.time()
+
+        while True:
+            logger_cnt += 1
+
+
+            # * * * E-step: compute posterior * * *
+            expectation_start_time = time.time()
+            batch_log_pi = self.get_batch_log_pi(self.train_trajs, agents) # (B, K, T)
+
+            e_dataset = TensorDataset(batch_states, batch_actions, batch_log_pi, batch_mask)
+            e_loader = DataLoader(e_dataset, batch_size=1024, shuffle=False)
+            log_p_gammas, *_ = self.intention_batch_mapping(e_loader, max_len)
+            batch_target_gamma = torch.exp(log_p_gammas).detach()
+            expectation_time = time.time() - expectation_start_time
+            logstep_exp_time += expectation_time
+
+
+            # * * * Update Q-value & policies * * *
+            m_dataset = TensorDataset(batch_states, batch_actions, batch_target_gamma, batch_mask)
+            m_loader = DataLoader(m_dataset, batch_size=256, shuffle=True)
+
+            q_start_time = time.time()
+            for latent_idx in range(self.num_latents):
+                expert_pi = torch.zeros((self.num_states, self.num_actions))
+                for traj_idx, traj in enumerate(self.train_trajs):
+                    weights = batch_target_gamma[traj_idx][:, latent_idx]
+                    for t, (s, a, ns) in enumerate(traj):
+                        expert_pi[s, a] += weights[t]
+                mask = expert_pi.sum(dim=1) == 0
+                expert_pi[mask] = 1e-6
+                expert_pi /= expert_pi.sum(dim=1, keepdim=True)
+
+                agent = IAVI_B(
+                    num_states=self.num_states,
+                    num_actions=self.num_actions,
+                    P=self.P,
+                    expert_policy=expert_pi.numpy(),
+                    discount=self.discount,
+                    device=self.device
+                )
+                agent.train()
+                agents[latent_idx] = agent
+            q_time = time.time() - q_start_time
+            logstep_q_time += q_time
+
+            # * * * Update intention network * * *
+            intention_start_time = time.time()
+            total_loss = self.train_minibatch(m_loader, max_len, num_epochs=self.num_epochs,
+                                              reg_weight=self.reg_weight, reg_type=self.reg_type)
+            intention_time = time.time() - intention_start_time
+            logstep_intention_time += intention_time
+
+            self.target_intention_net.load_state_dict(self.intention_net.state_dict())
+
+            if logger_cnt % 3 == 0:
+                iteration_time = time.time() - iteration_start_time
+                print(f'Iteration {logger_cnt}, Loss: {total_loss:.4f}, \n\
+                       \tExpectation: {logstep_exp_time:.2f}s, Q-update: {logstep_q_time:.2f}s, Intention: {logstep_intention_time:.2f}s, \n\
+                       \tTotal: {int(iteration_time // 60):d}m, {int(iteration_time % 60):d}s')
+                logstep_exp_time = 0
+                logstep_q_time = 0
+                logstep_intention_time = 0
+
+            if (abs(total_loss) < self.loss_threshold) or (logger_cnt >= self.max_iterations):
+                final_iteration_time = time.time() - iteration_start_time
+                print(f'Iteration {logger_cnt}, Converged with Loss: {total_loss:.4f}, Total time: {final_iteration_time:.2f}s')
+                break
+
+        f = {}
+        ll = {}
+        mask = {}
+        for ds in ['train', 'test']:
+            trajs = self.train_trajs if ds == 'train' else self.test_trajs
+            batch_states_eval, batch_actions_eval, mask_eval = self.encode_batch_trajs(trajs)
+            max_len_eval = batch_states_eval.shape[1]
+            batch_log_pi_eval = self.get_batch_log_pi(trajs, agents)
+
+            eval_dataset = TensorDataset(batch_states_eval, batch_actions_eval, batch_log_pi_eval, mask_eval)
+            eval_loader = DataLoader(eval_dataset, batch_size=1024, shuffle=False)
+            _, log_f, log_p_joint = self.intention_batch_mapping(eval_loader, max_len_eval)
+            
+            # Get per-trajectory results
+            fs = []
+            lls = []
+            for i, seq_len in enumerate(mask_eval.sum(dim=1)):
+                f_i = torch.exp(log_f[i, :, :]).cpu().numpy()
+                log_joint_i = log_p_joint[i, :seq_len, :].cpu().numpy()
+                fs.append(f_i)
+                lls.append(np.mean(logsumexp(log_joint_i, axis=-1)))
+            lls = np.mean(np.hstack(lls))
+            ll[ds] = np.mean(lls)
+            f[ds] = fs
+            mask[ds] = mask_eval.cpu().numpy()
+
+        return ll, f, mask, agents
+
+    def predict(self, trajs, agents):
+        batch_states, batch_actions, mask = self.encode_batch_trajs(trajs)
+        max_len = batch_states.shape[1]
+        batch_log_pi = self.get_batch_log_pi(trajs, agents)
+
+        eval_dataset = TensorDataset(batch_states, batch_actions, batch_log_pi, mask)
+        eval_loader = DataLoader(eval_dataset, batch_size=1024, shuffle=False)
+        _, log_f, log_p_joint = self.intention_batch_mapping(eval_loader, max_len)
+
+        fs = []
+        lls = []
+        for i, seq_len in enumerate(mask.sum(dim=1)):
+            f_i = torch.exp(log_f[i, :seq_len, :]).cpu().numpy()
+            log_joint_i = log_p_joint[i, :seq_len, :].cpu().numpy()
+            fs.append(f_i)
+            lls.append(np.mean(logsumexp(log_joint_i, axis=-1)))
+        lls = np.mean(np.hstack(lls))
+        
+        return np.mean(lls), fs
